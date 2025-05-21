97514aeb8029746205cf5abcdcbd0a4ac0ea6bb1
asisayag2 <asi@cloudinary.com>
2025-05-21 18:35:02 +0300

    Add server.listen() to properly start the server

--- server/server.js
+++ server/server.js
@@ -120,3 +120,8 @@ io.on('connection', (socket) => {
   });
 });
 
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
